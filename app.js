bf392365d82c02d6cf7f56db36cfee9b7760f2ed
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-04-23 09:44:12 +0800

    navbar fixed

--- app.js
+++ app.js
@@ -17,10 +17,31 @@ date.innerHTML = new Date().getFullYear();
 
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
-const link = document.querySelector(".links");
+const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
   //   linksContainer.classList.toggle("show-links");
-  const containerHeight = linksContainer.getBoundingClientRect();
-  console.log(containerHeight);
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+// fixed navbar
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
 });
